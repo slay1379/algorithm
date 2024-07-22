a9624e64f99d434cb746b78ce1a425dbcf21d1b2
slay1379 <65012092+slay1379@users.noreply.github.com>
2024-07-22 17:04:34 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 64 ms, Memory: 35668 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+from itertools import combinations
+from collections import deque
+from collections import defaultdict
+import heapq
+
+
+T = int(input())
+for _ in range(T):
+    A,B = map(int,input().split())
+    print("yes")
+
