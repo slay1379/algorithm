e57b3a607d0ad86d1c719e563aaacd06fd54ab3e
slay1379 <65012092+slay1379@users.noreply.github.com>
2024-07-28 19:51:59 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+dp=[0]*101
+dp[1]=1
+dp[2]=1
+dp[3]=1
+for i in range(4,101):
+    dp[i] = dp[i-2]+dp[i-3]
+for i in range(T):
+    N=int(input())
+    print(dp[N])
