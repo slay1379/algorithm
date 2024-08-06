8dce9dfcb867996661fd614616c1677c3314fc91
slay1379 <65012092+slay1379@users.noreply.github.com>
2024-08-06 18:33:10 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 50928 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N = int(input())
+card = deque()
+for i in range(1,N+1):
+    card.append(i)
+
+while len(card)>1:
+    card.popleft()
+    card.append(card.popleft())
+
+print(card[0])
