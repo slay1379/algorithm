29b0f953f1a501d78b188761da518628a78b6193
slay1379 <65012092+slay1379@users.noreply.github.com>
2024-09-18 14:18:07 +0900

    [Gold V] Title: 토마토, Time: 972 ms, Memory: 101720 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,37 +1,46 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+from itertools import combinations
+from collections import deque
+from collections import defaultdict
+import heapq
+
+def tomato():
+    q = deque()
+    for i in range(N):
+        for j in range(M):
+            if box[i][j] == 1:
+                q.append((i,j))
+
+    dir = [(-1,0),(1,0),(0,1),(0,-1)]
+
+    while q:
+        x,y = q.popleft()
+        for dx,dy in dir:
+            nx = x+dx
+            ny = y+dy
+            if 0<=nx<N and 0<=ny<M:
+                if box[nx][ny] == 0:
+                    box[nx][ny] = box[x][y]+1
+                    q.append((nx,ny))
+
+
 
 M,N = map(int,input().split())
-box = [list(map(int,input().split()))for _ in range(N)]
+box = [list(map(int,input().split())) for _ in range(N)]
+answer = 0
 
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-q=deque()
+tomato()
 
 for i in range(N):
     for j in range(M):
-        if box[i][j] == 1:
-            q.append((i,j))
-def BFS():
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = dx[i]+x
-            ny = dy[i]+y
-            if nx < 0 or ny < 0 or nx >= N or ny >= M:
-                continue
-            if box[nx][ny] == 0:
-                q.append((nx,ny))
-                box[nx][ny] = box[x][y]+1
+        if box[i][j] == 0:
+            print(-1)
+            exit()
 
-BFS()
-max_day = 0
 for row in box:
-    if 0 in row:
-        print('-1')
-        exit(0)
-    max_day = max(max_day, max(row))
+    if max(row) > answer:
+        answer = max(row)
 
-print(max_day-1)
+print(answer-1)
