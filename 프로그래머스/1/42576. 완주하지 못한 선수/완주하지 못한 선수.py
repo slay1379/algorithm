fb003538fd25381a772a165b0fcbe169ab0eaab8
slay1379 <65012092+slay1379@users.noreply.github.com>
2025-02-07 18:49:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.89 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
