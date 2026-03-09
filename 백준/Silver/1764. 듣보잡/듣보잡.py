09ab086ffa7cbd6f40a870a12eed8f98f5d96b92
김태헌 <65012092+slay1379@users.noreply.github.com>
2026-03-09 18:58:36 +0900

    [Silver IV] Title: 듣보잡, Time: 88 ms, Memory: 46616 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,11 +1,14 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
+import heapq
 
-N, M = map(int,input().split())
+N,M = map(int,input().split())
 not_listen = {input().strip() for _ in range(N)}
 not_see = {input().strip() for _ in range(M)}
 
-result = sorted(list(not_listen & not_see))
+not_listen_and_see = not_listen & not_see
+result = sorted(not_listen_and_see)
 
 print(len(result))
 for name in result:
