446b76cb4a23a43ddc4ef485f2e5caf04e715030
김태헌 <65012092+slay1379@users.noreply.github.com>
2026-03-30 19:43:43 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 43928 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,29 +1,22 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-from itertools import combinations
 from collections import deque
 from collections import defaultdict
 import heapq
+from itertools import combinations
 
 N,K = map(int,input().split())
-degree = list(map(int,input().split()))
+degrees = list(map(int,input().split()))
 
-total = 0
-
-left,right = 0,K-1
-for i in range(K):
-    total += degree[i]
-
-answer = total
-
-while right < N-1:
-    total -= degree[left]
-    left += 1
-    right += 1
-    total += degree[right]
+answer = sum(degrees[:K])
+total = answer
+left, right = 0, K-1
+while left < N-K:
+    total -= degrees[left]
+    total += degrees[right+1]
     if answer < total:
         answer = total
+    left += 1
+    right += 1
 
 print(answer)
-
