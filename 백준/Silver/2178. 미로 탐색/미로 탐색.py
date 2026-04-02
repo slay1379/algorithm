a43e9964365f6ac293a0393ae095668bcafd48b1
김태헌 <65012092+slay1379@users.noreply.github.com>
2026-04-02 22:23:50 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 36240 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,28 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
+from collections import defaultdict
+import heapq
+from itertools import combinations
+from collections import Counter
 
-N,M = map(int,input().split())
-miro = [list(map(int,input().strip())) for _ in range(N)]
-visit = [[False]*M for _ in range(N)]
+N, M = map(int,input().split())
+board = [list(map(int,input().strip())) for _ in range(N)]
+
 q = deque()
 q.append((0,0))
-visit[0][0] = True
-dir = [(1,0),(-1,0),(0,1),(0,-1)]
+nx = [-1,1,0,0]
+ny = [0,0,1,-1]
+visit = [[-1] * M for _ in range(N)]
+visit[0][0] = 1
 
 while q:
     x,y = q.popleft()
-    for dx,dy in dir:
-        nx,ny = x+dx, y+dy
-        if 0<=nx<N and 0<=ny<M:
-            if miro[nx][ny] == 1 and not visit[nx][ny]:
-                q.append((nx,ny))
-                visit[nx][ny] = True
-                miro[nx][ny] = miro[x][y] + 1
+    for i in range(4):
+        dx = x+nx[i]
+        dy = y+ny[i]
+        if 0<=dx<N and 0<=dy<M and board[dx][dy] == 1 and visit[dx][dy] == -1:
+            q.append((dx,dy))
+            visit[dx][dy] = visit[x][y] + 1
 
-print(miro[-1][-1])
-
+print(visit[N-1][M-1])
