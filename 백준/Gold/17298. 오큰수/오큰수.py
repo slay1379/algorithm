86c1d2c560c30f4250cb3689880fd3b7597193bf
김태헌 <65012092+slay1379@users.noreply.github.com>
2026-04-04 00:50:22 +0900

    [Gold IV] Title: 오큰수, Time: 1172 ms, Memory: 144792 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,20 +1,19 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-from itertools import combinations
 from collections import deque
 from collections import defaultdict
 import heapq
-
+from itertools import combinations
+from collections import Counter
 
 N = int(input())
 A = list(map(int,input().split()))
-answer = [-1]*N
+result = [-1]*N
 stack = []
 
 for i in range(N):
     while stack and A[stack[-1]] < A[i]:
-        answer[stack.pop()] = A[i]
+        result[stack.pop()] = A[i]
     stack.append(i)
 
-print(' '.join(map(str,answer)))
+print(*result)
