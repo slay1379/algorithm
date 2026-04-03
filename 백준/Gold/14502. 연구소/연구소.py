193e62f92253d109e21c82ba2fcbc3378fce2126
김태헌 <65012092+slay1379@users.noreply.github.com>
2026-04-03 23:02:48 +0900

    [Gold IV] Title: 연구소, Time: 1520 ms, Memory: 36828 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,63 +1,70 @@
 import sys
-from collections import deque
-import copy
-
 input = sys.stdin.readline
+from collections import deque
+from collections import defaultdict
+import heapq
+from itertools import combinations
+from collections import Counter
 
-N, M = map(int,input().split())
+N,M = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(N)]
-dir = [(1,0),(-1,0),(0,1),(0,-1)]
+answer = []
 
-def progress_virus(board):
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+virus = []
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 2:
+            virus.append((i,j))
+
+def bfs():
     q = deque()
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 2:
-                q.append((i,j))
+    for i,j in virus:
+        q.append((i,j))
+
+    new_board = [row[:] for row in board]
 
     while q:
         x,y = q.popleft()
-        for nx,ny in dir:
-            dx = x+nx
-            dy = y+ny
-            if 0<=dx<N and 0<=dy<M:
-                if board[dx][dy] == 0:
-                    board[dx][dy] = 2
-                    q.append((dx,dy))
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<N and 0<=ny<M and new_board[nx][ny] == 0:
+                q.append((nx,ny))
+                new_board[nx][ny] = 2
 
-    return count_zero(board)
-
-def count_zero(board):
-    count = 0
+    result = 0
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 0:
-                count+=1
-    return count
+            if new_board[i][j] == 0:
+                result += 1
 
-answer = 0
+    return result
 
-zeros = []
+def dfs(x,y,cnt):
+    if cnt == 3:
+        answer.append(bfs())
+        return
+    i,j = x,y
+    while True:
+        if i == N-1 and j == M-1:
+            break
+        j += 1
+        if j == M:
+            i += 1
+            j = 0
+        if board[i][j] == 0:
+            board[i][j] = 1
+            dfs(i,j,cnt+1)
+            board[i][j] = 0
+
 for i in range(N):
     for j in range(M):
         if board[i][j] == 0:
-            zeros.append((i,j))
+            board[i][j] = 1
+            dfs(i,j,1)
+            board[i][j] = 0
 
-for i in range(len(zeros)-2):
-    for j in range(i+1,len(zeros)-1):
-        for k in range(j+1,len(zeros)):
-            if board[zeros[i][0]][zeros[i][1]] != 0 or board[zeros[j][0]][zeros[j][1]] != 0 or board[zeros[k][0]][zeros[k][1]] != 0:
-                continue
-
-            board[zeros[i][0]][zeros[i][1]] = 1
-            board[zeros[j][0]][zeros[j][1]] = 1
-            board[zeros[k][0]][zeros[k][1]] = 1
-            temp_board = copy.deepcopy(board)
-            cnt = progress_virus(temp_board)
-            if cnt > answer:
-                answer = cnt
-            board[zeros[i][0]][zeros[i][1]] = 0
-            board[zeros[j][0]][zeros[j][1]] = 0
-            board[zeros[k][0]][zeros[k][1]] = 0
-
-print(answer)
+print(max(answer))
